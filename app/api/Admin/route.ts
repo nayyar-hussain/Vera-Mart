c654cdf7043519c491c30048ce26780130cde576
Nayyar Hussain <nayyarhussain125@gmail.com>
2026-01-13 18:51:02 +0500

    extra delete put removed from Admin Api

--- app/api/Admin/route.ts
+++ app/api/Admin/route.ts
@@ -16,22 +16,4 @@ export async function POST(req: NextRequest) {
 
 }
 
-export async function DELETE(req:NextRequest, {params} : {params : {id : string}}) {
-    const productId = params.id
-    if(!productId){
-        return NextResponse.json({status : 404})
-    }
-    await Product.findByIdAndDelete(productId)
-    return NextResponse.json({status : 200})
-}
 
-export async function PUT(req:NextRequest, {params}: {params : {id : string}}) {
-    const productId = params.id
-    const {name , description , imageUrl , price} = await req.json()
-        if(!name || !description || !imageUrl || !price){
-        return NextResponse.json({status : 404})
-    }
-
-    await Product.findByIdAndUpdate(productId , {name , description , imageUrl , price}, {new : true})
-    return NextResponse.json({status : 200})
-}
